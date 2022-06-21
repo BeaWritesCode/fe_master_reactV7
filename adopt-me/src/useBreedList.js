ede419439e2cfa39a841e2736909a6703e21c2e5
BeaWritesCode <36712281+BeaWritesCode@users.noreply.github.com>
2022-06-21 18:22:25 +0100

    Add custom hook for breeds

---
+++ adopt-me/src/useBreedList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const localCache = {}; // Would really be localStorage or something
+// but this is just a tutorial so make an object instead
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
